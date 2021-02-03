e0dcf88dc33edf7b9cdf29a06b2d1f0fb925c8a4
Derrick Cassidy <decassidy@icloud.com>
2021-02-03 22:38:36 +0000

    commit variable-naming.sh

---
+++ variable-naming.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+########################################
+# Author: Derrick Cassidy
+# Version: v1.0.0
+# Date: 2021-02-03
+# Description: Showing off different styles of variable naming.
+# Usage: ./variable-naming.sh
+########################################
+
+# Assign the variables
+NAME="Derrick"
+HOME_TYPE="house"
+LOCATION="Barbados"
+PARTNER_NAME="Uka"
+ANIMAL_TYPES="gecko and hamster"
+
+# Print the story
+echo "${NAME} lives in a ${HOME_TYPE} in ${LOCATION}, together with ${PARTNER_NAME} and their two pets: a ${ANIMAL_TYPES}."
