a5300b9a7b858627714ec691d9a20aeeb950de3d
Derrick Cassidy <decassidy@icloud.com>
2021-02-03 21:55:11 +0000

    Added script header to main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+################################
+# Author: Derrick Cassidy
+# Version: 1.0.0
+# Date: 2021-02-03
+# Description: Bash Sandbox
+# Usage: ./main.sh
+################################
+
+# Print text to the Terminal
 echo "Hello World!"
